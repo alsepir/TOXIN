7563628e32d979ba085cfeb3ca7d00f77eb82863
Alexandr Pirogov <52838932+alsepir@users.noreply.github.com>
2020-01-08 15:20:48 +0300

    First pug config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // Переменное окружение - дев/прод в node.js
 const NODE_ENV = process.env.NODE_ENV || 'development';
+
+// Pages const  for HtmlWebpackPlugin
+//const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+//const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
 
 module.exports = {
   mode: 'development',
@@ -37,6 +42,10 @@ module.exports = {
     new webpack.DefinePlugin({
       NODE_ENV: JSON.stringify(NODE_ENV), // use 'development' unless process.env.NODE_ENV is defined
       DEBUG: false
+    }),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: './src/page/index.pug'
     })
   ],
 
@@ -51,6 +60,10 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader'
       }
     ]
   }
