c863986d1ec457fc3c60551bf273c173ed8d1fa9
Alexandr Pirogov <52838932+alsepir@users.noreply.github.com>
2020-01-09 21:46:11 +0300

    Make color square for UI kit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import './style.css';
+import './assets/css/style.css';
+
+function component() {
+  const element = document.createElement('div');
+  element.classList.add('square');
+  return element;
+}
+
+for(let i = 0; i < 7; i++) {
+  document.body.appendChild(component());
+}
