9a4d0af6418265f760bb945a5332443c93fdb8e2
Alexandr Pirogov <52838932+alsepir@users.noreply.github.com>
2020-01-14 00:04:31 +0300

    Add advantage block

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './assets/scss/style.scss';
 import Logo from './logo.svg';
+import Emotion from './advantage-emoticon.svg';
+import City from './advantage-city.svg';
 
 
 function component() {
