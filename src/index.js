e836a2467a0ee78a7212601ad7305062df9c4173
Alexandr Pirogov <52838932+alsepir@users.noreply.github.com>
2020-01-16 23:28:05 +0300

    Add custom stlyle for selectmenu

--- src/index.js
+++ src/index.js
@@ -4,45 +4,53 @@ import Emotion from './advantage-emoticon.svg';
 import City from './advantage-city.svg';
 
 require('jquery');
-require("jquery-ui/ui/widgets/draggable");
-require("jquery-ui/ui/widgets/dialog");
+//require('jquery-ui/ui/widgets/draggable');
+require('jquery-ui/ui/widgets/datepicker.js');
+require('jquery-ui/themes/base/datepicker.css');
+import 'jquery-ui/ui/widgets/selectmenu.js';
+//import 'jquery-ui/themes/base/selectmenu.css';
+import './jquery-ui.css'
 
 $(".test").css("background", "yellow");
-
-$(".test").draggable(); // эта строка кода, которая делает элемент перетаскиваемым
+$(".test").datepicker();
+$('#speed').selectmenu();
+$( "#speed" ).selectmenu( "open" );
+let speed = $( "#speed" ).selectmenu( "menuWidget" );
+console.log(speed);
+//$(".test").draggable(); // эта строка кода, которая делает элемент перетаскиваемым
 
 // Зададим плагин cDialog, наследуемый от dialog
-$.widget("my.cDialog", $.ui.dialog, {
+// $.widget("my.cDialog", $.ui.dialog, {
 
-  // настройки dialog автоматически перейдут cDialog,
-  // дополнительные настройки зададим позже
-  options: {
+//   // настройки dialog автоматически перейдут cDialog,
+//   // дополнительные настройки зададим позже
+//   options: {
 
-  },
+//   },
 
-  // конструктор плагина.
-  _create: function() {
-    // вызовем в нем конструктор родительского плагина
-    $.ui.dialog.prototype._create.call(this);
+//   // конструктор плагина.
+//   _create: function() {
+//     // вызовем в нем конструктор родительского плагина
+//     $.ui.dialog.prototype._create.call(this);
 
-    // в дальнейшем, мы разместим здесь и свои манипуляции
-  },
+//     // в дальнейшем, мы разместим здесь и свои манипуляции
+//   },
 
-  // деструктор плагина
-  destroy: function() {
-    // вызовем в нем деструктор родительского плагина
-    $.ui.dialog.prototype.destroy.call(this);
-  },
+//   // деструктор плагина
+//   destroy: function() {
+//     // вызовем в нем деструктор родительского плагина
+//     $.ui.dialog.prototype.destroy.call(this);
+//   },
 
-  // обработка изменения свойств
-  _setOption: function() {
-    $.ui.dialog.prototype._setOption.apply( this, arguments );
-  }  
+//   // обработка изменения свойств
+//   _setOption: function() {
+//     $.ui.dialog.prototype._setOption.apply( this, arguments );
+//   }  
 
-});
+// });
 
-// установим плагин cDialog на элемент с id = wnd
-$("#wnd").cDialog();
+// // установим плагин cDialog на элемент с id = wnd
+// $("#wnd").cDialog();
 
 function component() {
   const element = document.createElement('div');
