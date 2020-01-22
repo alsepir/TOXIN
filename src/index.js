af3adfa14d94c8b4bfd060517c48848741ed0aad
Alexandr Pirogov <52838932+alsepir@users.noreply.github.com>
2020-01-22 22:39:53 +0300

    Add header in UIkit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './assets/scss/style.scss';
 import Logo from './logo.svg';
 import Emotion from './advantage-emoticon.svg';
 import City from './advantage-city.svg';
+import './Logo-grad.svg'
 
 // require('jquery');
 // //require('jquery-ui/ui/widgets/draggable');
