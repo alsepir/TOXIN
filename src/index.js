6c9fefc76f69561091fdd2ff4aef22aaa5f13de7
Alexandr Pirogov <52838932+alsepir@users.noreply.github.com>
2020-01-25 21:20:13 +0300

    Create footer component
    Make responsive with grid and flexbox,
    first state layout block in footer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import './assets/scss/style.scss';
+//import './assets/scss/style.scss';
+import './components/header/header.scss';
+import './components/footer/footer.scss';
+
+import './image-3.png';
+
+
 import Logo from './logo.svg';
 import Emotion from './advantage-emoticon.svg';
 import City from './advantage-city.svg';
