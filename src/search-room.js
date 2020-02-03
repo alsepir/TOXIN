7658c3d33700621b6600e96d992a25aaa937a0c2
Alexandr Pirogov <52838932+alsepir@users.noreply.github.com>
2020-02-03 22:17:30 +0300

    room-frame transform to mixin for runtime prod.

--- src/search-room.js
+++ src/search-room.js
@@ -1,6 +1,18 @@
-import './components/room-frame/image-min.png';
-import './components/room-frame/image-2.png';
-import './components/room-frame/image-3.png';
+import './components/room-frame/image/888/image-min.png';
+import './components/room-frame/image/888/image-2.png';
+import './components/room-frame/image/888/image-3.png';
+
+import './components/room-frame/image/840.jpg';
+import './components/room-frame/image/980.jpg';
+import './components/room-frame/image/856.jpg';
+import './components/room-frame/image/740.jpg';
+import './components/room-frame/image/982.jpg';
+import './components/room-frame/image/678.jpg';
+import './components/room-frame/image/450.jpg';
+import './components/room-frame/image/350.jpg';
+import './components/room-frame/image/666.jpg';
+import './components/room-frame/image/444.jpg';
+import './components/room-frame/image/352.jpg';
 
 import './components/room-frame/room-frame.scss';
 
