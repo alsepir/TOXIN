2684e5b5f71ed9e2f27693b68bac8ff39ab77b9b
Alexandr Pirogov <52838932+alsepir@users.noreply.github.com>
2020-03-10 21:48:58 +0300

    checkbox-buttons transform to class component

--- src/components/checkbox-buttons/checkbox-buttons.js
+++ src/components/checkbox-buttons/checkbox-buttons.js
@@ -1,22 +1,47 @@
-// class CheckboxButtons {
-//   bindEventListeners() {
-//     stopButton.addEventListener('click', this.handleStopButtonClick);
-//   }
-
-//   handleStopButtonClick() {
-//     square.classList.toggle("active");
-//   }
-// }
-
 import './checkbox-buttons.scss';
 
-(function() {
-  let checkboxs = document.querySelectorAll(".checkbox-list .item");
+class CheckboxList {
+	constructor(id, component) {
+		this.id = id;
+		this.button = component.querySelector(".checkbox-list__head");
+		this.buttonIsActive = component.querySelector(".checkbox-list__head_active");
+		this.list = component.querySelector(".checkbox-list__items");
 
-  for(let square of checkboxs) {
-    square.onclick = function() {
-      console.dir(square)
-      square.classList.toggle("active");
-    }
-  }
+		this._handleButtonClick = this._handleButtonClick.bind(this); 
+	}
+
+	addEventListeners() {
+		if(this.buttonIsActive)
+			this.button.addEventListener("click", this._handleButtonClick);
+
+		this.list.addEventListener("click", this._handleBoxClick);
+	}
+
+	_handleBoxClick(evt) {
+		if(evt.target.classList[0] === "checkbox-list__box") {
+			evt.target.classList.toggle("checkbox-list__box_active");
+		}
+	}
+
+	_handleButtonClick(evt) {
+		if(evt.target.classList[0] === "checkbox-list__head") {
+			this.list.classList.toggle("checkbox-list__items_hide");
+		}
+	}
+}
+
+let checkboxLists = (function() {
+	let checkboxButtons = document.querySelectorAll(".checkbox-list");
+
+	let id = 0;
+	let newListItems = [];
+	for(let item = 0; item < checkboxButtons.length; item++) {
+		let newListItem = new CheckboxList(id++, checkboxButtons[item]);
+		newListItem.addEventListeners();
+		newListItems.push(newListItem);
+	}
+
+	return newListItems;
 })();
+
+console.log(checkboxLists);
