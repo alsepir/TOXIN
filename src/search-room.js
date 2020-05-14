dceaa6465de2c275c658296517afff81441132c2
Alexandr Pirogov <52838932+alsepir@users.noreply.github.com>
2020-05-14 23:03:18 +0300

    Created pagination component

--- src/search-room.js
+++ src/search-room.js
@@ -6,3 +6,4 @@ import './assets/page-layout/search-room.scss';
 // Подключение используемых компонентов
 import './components/room-frame/slideshow.js';
 import './components/checkbox-list/checkbox-list.js';
+import './components/pagination/pagination.js'
